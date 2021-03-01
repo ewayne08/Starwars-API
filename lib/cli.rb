4464b1d55b6a799682127b0272efdb693213f15d
Ethan <ethanchristianwayne@gmail.com>
2021-03-01 14:34:26 -0500

    message

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,4 +2,11 @@
 require 'pry'
 
 class Cli  
-    
+    def start
+        puts "
+        This is a galaxy filled with wonder, unknown, science, bizarre creatures, 
+             aliens, weapons never seen before by man, terrible people
+                   galaxy full of unknown but fantastic worlds, 
+                     magical devices, fleets of unknown space
+                        crafts, terrible conflict, and 
+                               unending hope."
